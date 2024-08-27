75b0dd776042a3bde3746f490fcd46166717890a
vorexele <ali.alaskarovdev@gmail.com>
2024-08-27 12:32:25 +0200

    electron cavcav romantichniy

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // Load the index.html of the app.
+  win.loadFile('index.html');
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it’s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
